9bce5fce7f0ef05da947082deb672d1ac6b2e76e
sorawaslocked <133684349+sorawaslocked@users.noreply.github.com>
2024-09-08 04:57:07 +0500

    Add DbContext and override OnModelCreating

---
+++ Quizum.Dal/EfStructures/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿namespace Quizum.Dal.EfStructures;
+
+public class AppDbContext
+    : IdentityDbContext<AppUser, AppRole, int, AppUserClaim, AppUserRole, AppUserLogin, AppRoleClaim, AppUserToken>
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        
+        new AppRoleClaimConfiguration().Configure(builder.Entity<AppRoleClaim>());
+        new AppRoleConfiguration().Configure(builder.Entity<AppRole>());
+        new AppUserClaimConfiguration().Configure(builder.Entity<AppUserClaim>());
+        new AppUserConfiguration().Configure(builder.Entity<AppUser>());
+        new AppUserLoginConfiguration().Configure(builder.Entity<AppUserLogin>());
+        new AppUserRoleConfiguration().Configure(builder.Entity<AppUserRole>());
+        new AppUserTokenConfiguration().Configure(builder.Entity<AppUserToken>());
+    }
+}
---
+++ Quizum.Dal/GlobalUsings.cs
@@ -0,0 +1,15 @@
+﻿global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Design;
+
+global using Microsoft.AspNetCore.Identity;
+global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+
+global using Quizum.Dal.EfStructures;
+
+global using Quizum.Models.Entities;
+global using Quizum.Models.Entities.Identity;
+global using Quizum.Models.Entities.Configuration;
+global using Quizum.Models.Entities.Configuration.Identity;
