a86984b73c9546f3771458548b5c0097aef01f45
sorawaslocked <133684349+sorawaslocked@users.noreply.github.com>
2024-09-08 05:21:00 +0500

    Add DbContextFactory for migrations

---
+++ Quizum.Dal/EfStructures/AppDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿namespace Quizum.Dal.EfStructures;
+
+public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+        var connectionString = configuration.GetConnectionString("Quizum");
+        optionsBuilder.UseNpgsql(connectionString);
+
+        return new AppDbContext(optionsBuilder.Options);
+    }
+}
