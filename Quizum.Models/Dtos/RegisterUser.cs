4d78df31c2714195d1fb6278e377ce7b5b5c4ee8
sorawaslocked <133684349+sorawaslocked@users.noreply.github.com>
2024-09-08 05:35:37 +0500

    Add register user dto

---
+++ Quizum.Models/Dtos/RegisterUser.cs
@@ -0,0 +1,10 @@
+﻿namespace Quizum.Models.Dtos;
+
+public class RegisterUser
+{
+    public string UserName { get; init; }
+    public string Email { get; init; }
+    public string Password { get; init; }
+    public string FirstName { get; init; }
+    public string LastName { get; init; }
+}
